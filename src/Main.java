a7cf485e4ab0fedd93348014dbb69ab62bcb16ad
AmNotAGoose <alexyihengwu@gmail.com>
2025-06-10 10:38:54 -0400

    added display

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,71 @@
+import javax.swing.*;
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-    static String s = "";
+    static Integer[][] board = new Integer[6][7];
 
     public static void displayMessageBox(String message) {
-        // Clear the screen
-        for (int i = 0; i < 10; i++) {
-            System.out.println();
-        }
 
-        System.out.println(message);
     }
 
     public static void displayMainMenu() {
-        s = "213ew";
-        System.out.println(s);
+
+    }
+
+    public static void displayBoard() {
+        for (int i = 0; i < board.length; i++) {
+            System.out.print("|");
+            for (int j = 0; j < board[0].length; j++) {
+                switch (board[i][j]) {
+                    case 0: // empty
+                        System.out.print("  |");
+                        break;
+                    case 1: // X
+                        System.out.print(" X |");
+                        break;
+                    case 2: // O
+                        System.out.print(" O |");
+                        break;
+                    default: // error
+                        System.out.print(" ? |");
+                        break;
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void initializeBoard() {
+        // initialize board
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] = 0;
+            }
+        }
+    }
+
+    public static void checkForMatches() {
+        // check horizontally
+        // check vertically
+        // check diagonally
+    }
+
+    public static void PVP() {
+        boolean p1Won = false;
+        boolean p2Won = false;
+
+
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        displayMainMenu();
-        displayMessageBox("hawk");
+
+        initializeBoard();
+        displayBoard();
     }
-    // hello ms mileti
 }
 
 
