9694eabd245af10de135b52040c042661e287fdf
AmNotAGoose <alexyihengwu@gmail.com>
2025-06-10 09:48:36 -0400

    hello ms mileti

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.ArrayList;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static String s = "";
+
+    public static void displayMessageBox(String message) {
+        // Clear the screen
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
         }
+
+        System.out.println(message);
     }
+
+    public static void displayMainMenu() {
+        s = "213ew";
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        displayMainMenu();
+        displayMessageBox("hawk");
+    }
+    // hello ms mileti
 }
+
+
